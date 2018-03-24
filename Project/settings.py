d39874b6520f6209db1ae60d1c0a7b5c7fb4461f
tunir27 <42wolf@gmail.com>
2018-03-24 23:54:01 +0530

    First Production Commit

--- Project/settings.py
+++ Project/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
