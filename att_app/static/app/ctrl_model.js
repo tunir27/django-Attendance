46201d62db1183387a976cc1f2dc579d720e9e2f
tunir27 <42wolf@gmail.com>
2018-03-25 02:17:26 +0530

    API Details Updated

--- att_app/static/app/ctrl_model.js
+++ att_app/static/app/ctrl_model.js
@@ -1,12 +1,12 @@
 app.controller("ctrl_model",function($scope,$http){
 	url='https://attendanceproject.herokuapp.com/dashboard/apid/';
-	type="/?format=json";
+	//type="/?format=json";
 	//console.log("hello");
 	
 	$scope.fetch_details=function(id){
 		console.log(id);
 		$('#myModal').modal('show');
-		$http.get(url+id+type).success(function(data){
+		$http.get(url+id).success(function(data){
 			$scope.s=data;
 			console.log(data);
 		})
