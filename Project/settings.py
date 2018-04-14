24922bffb062bc3d8ea8bb08c5d0afeb2af01777
tunir27 <42wolf@gmail.com>
2018-04-15 00:53:07 +0530

    Mail Settings Added

--- Project/settings.py
+++ Project/settings.py
@@ -123,6 +123,18 @@ USE_L10N = True
 USE_TZ = True
 
 
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = 'attendrteam@gmail.com'
+SERVER_EMAIL = 'attendrteam@gmail.com'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'attendrteam@gmail.com'
+EMAIL_HOST_PASSWORD = 'Tunir_1234'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+
+
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'att_app/static/')
